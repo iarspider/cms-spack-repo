e1f87cf80369a231ee30b35fb627d72fd60cd9bf
iarspider <iarspider@gmail.com>
2021-05-25 10:25:17 +0200

    Fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,9 @@ source spack/share/setup-env.sh
 echo Adding CMS repository
 spack repo add --scope=site ${SCRIPT_DIR}
 echo Adding external gcc 9.3.0
-cp config/compilers.yaml etc/spack/linux/compilers.yaml
+cp ${SCRIPT_DIR}/config/compilers.yaml etc/spack/linux/compilers.yaml
 echo Adding CMS hooks
-cp ${SCRIPT_DIR}/hook/* spack/lib/spack/spack/hooks/
+cp ${SCRIPT_DIR}/hook/* lib/spack/spack/hooks/
 echo Creating environment
 spack env create CMSSW_12_0_X ${SCRIPT_DIR}/config/CMSSW_12_0_X/spack.yaml
 echo Done
