27522e1b119e8384461f99570c0c4ce4f4fb0377
iarspider <iarspider@gmail.com>
2021-06-30 17:59:54 +0200

    Add mirror to 'site' scope, not to 'user'

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ source share/spack/setup-env.sh
 echo Adding CMS repository
 spack repo add --scope=site ${SCRIPT_DIR}
 echo Adding CMS mirror
-spack mirror add cms https://test-cms-spack.web.cern.ch/test-cms-spack/CMS/
+spack mirror add --scope=site cms https://test-cms-spack.web.cern.ch/test-cms-spack/CMS/
 echo Configuring spack
 cp ${SCRIPT_DIR}/config/config.yaml etc/spack/
 echo Adding external gcc 9.3.0
