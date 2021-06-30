183cb90a88af1595a8a080dd7d8e2eb1f6532736
iarspider <iarspider@gmail.com>
2021-06-30 11:01:44 +0200

    Add recipe for CORAL (#1)

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,9 @@ mkdir -p etc/spack/linux
 cp ${SCRIPT_DIR}/config/compilers.yaml etc/spack/linux/compilers.yaml
 echo Adding CMS hooks
 cp ${SCRIPT_DIR}/hook/* lib/spack/spack/hooks/
+echo Adding SCRAM build system support
+cp ${SCRIPT_DIR}/scram.py lib/spack/spack/build_systems/
+echo "from spack.build_systems.scram import ScramPackage" >> lib/spack/spack/pkgkit.py
 echo Creating environment
 spack env create CMSSW_12_0_X ${SCRIPT_DIR}/environments/CMSSW_12_0_X/spack.yaml
 echo Done
---
+++ packages/oracleocci-abi-hack-cms/package.py
@@ -0,0 +1,27 @@
+from spack import *
+import shutil
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../ToolfilePackage'))
+from scrampackage import write_scram_toolfile
+
+
+class OracleocciAbiHackCms(Package):
+    """An ABI hack to occi.h with std=c++17"""
+
+    homepage = "https://github.com/cms-sw"
+    url = "https://github.com/cms-sw/cms_oracleocci_abi_hack.git"
+
+    version('1.0.0', git = "https://github.com/cms-sw/cms_oracleocci_abi_hack.git", 
+             commit='88b2a965305226df1822a14af8fe7174ee5f1614')
+
+    depends_on('oracle-instant-client')
+
+    def install(self, spec, prefix):
+        make()
+        with working_dir('build', create=False):
+            shutil.copytree('lib',prefix.lib)
+            shutil.copytree('include',prefix.include)
+
+    def setup_environment(self, spack_env, run_env):
+        spack_env.set('INCLUDE_DIR','%s' % self.spec['oracle-instant-client'].prefix.include)
+        spack_env.set('LIB_DIR', '%s' % self.spec['oracle-instant-client'].prefix.lib)
