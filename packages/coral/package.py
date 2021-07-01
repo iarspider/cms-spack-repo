7af0e85cdf176c7e8229cd23ed6c09e43a5b3a14
iarspider <iarspider@gmail.com>
2021-07-01 11:30:03 +0200

    Update CORAL environment generation

--- packages/coral/package.py
+++ packages/coral/package.py
@@ -31,10 +31,6 @@ class Coral(ScramPackage):
     def __init__(self, spec):
         super().__init__(spec)
 
-        self.scram_arch = 'slc_amd64_gcc'
-        if sys.platform == 'darwin':
-            self.scram_arch = 'osx10_amd64_clang'
-
         self.cvssrc = 'coral'
         self.subpackageDebug = True
 
@@ -76,68 +72,20 @@ class Coral(ScramPackage):
             with open(join_path(prefix.etc.scramrc, 'coral.map'), 'w') as f:
                 f.write('{ucprojtype}=$SCRAM_ARCH/{pkgcategory}/{pkgname}/{ucprojtype}_*'.format(
                     ucprojtype=self.ucprojtype, pkgcategory='cms', pkgname='coral'))
-        # def install(self, spec, prefix):
-        # coral_version = 'CORAL.' + str(self.version)
-        # coral_u_version = coral_version.replace('.', '_')
 
-        # scram = Executable(spec['scram'].prefix.cli+'/scram')
-        # source_directory = self.stage.source_path
-        # scram_version = 'V%s' % spec['scram'].version
-
-        # config_tag = '%s' % spec['cmssw-config'].version.underscored
-        # with working_dir(self.stage.path):
-        # mkdirp('config')
-        # install_tree(source_directory, 'src')
-        # install_tree(join_path(spec['cmssw-config'].prefix), 'config')
-
-        # with open('config/config_tag', 'w') as f:
-        # f.write(config_tag+'\n' )
-
-        # uc=Executable('config/updateConfig.py')
-        # uc(  '-p', 'CORAL',
-        # '-v', '%s' % coral_u_version,
-        # '-s', '%s' % scram_version,
-        # '-t', '%s' % spec['coral-tool-conf'].prefix,
-        # '--keys', 'SCRAM_COMPILER=gcc',
-        # '--keys', 'PROJECT_GIT_HASH=' + coral_u_version)
-
-        # scram('--arch', '%s' % self.scram_arch, 'project', '-d', os.path.realpath(self.stage.path), '-b',
-        # 'config/bootsrc.xml')
-
-        # project_dir = os.path.realpath(join_path(self.stage.path, coral_u_version))
-        # with working_dir(project_dir, create=False):
-        # matches = []
-
-        # for f in glob('src/*/*/test/BuildFile*'):
-        # matches.append(f)
-        # for m in matches:
-        # if os.path.exists(m):
-        # os.remove(m)
-
-        # scram.add_default_env('LOCALTOP', project_dir)
-        # scram.add_default_env('CORAL_BASE', project_dir)
-        # scram.add_default_env('LD_LIBRARY_PATH', '%s/lib/%s' %
-        # (project_dir, self.scram_arch))
-        # scram('build', '-v', '-j8')
-        # shutil.rmtree('external')
-        # shutil.rmtree('tmp')
-        # os.remove('slc_amd64_gcc/python/LCG/PyCoral')
-        # install_tree(project_dir, prefix+'/'+coral_u_version)
-
-    def setup_dependent_environment(self, spack_env, run_env, dspec):
-        spack_env.set('CORAL_RELEASE_BASE', self.prefix)
-        spack_env.append_path('LD_LIBRARY_PATH', '%s/CORAL_%s/%s/lib' %
-                              (self.prefix, self.version.underscored, self.scram_arch))
-
-    def setup_environment(self, spack_env, run_env):
-        spack_env.set('LOCALTOP', self.prefix + '/' +
-                      self.version.underscored.string)
+    def setup_run_environment(self, spack_env):
+        # spack_env.set('LOCALTOP', self.prefix + '/' +
+        #               self.version.underscored.string)
         spack_env.set('CORAL_BASE', self.prefix)
-        spack_env.append_path('LD_LIBRARY_PATH', '%s/CORAL_%s/%s/lib' %
-                              (self.prefix, self.version.underscored, self.scram_arch))
+        spack_env.append_path('LD_LIBRARY_PATH', '%s/%s/lib' %
+                              (self.prefix,  self.cmsplatf))
+        spack_env.append_path('PYTHONPATH', '%s/%s/lib' %
+                              (self.prefix, self.cmsplatf))
+        spack_env.append_path('PYTHONPATH', '%s/%s/python' %
+                              (self.prefix, self.cmsplatf))
 
     # @run_after('install')
     # def make_links(self):
     # with working_dir(self.spec.prefix):
     # os.symlink('CORAL_%s/include/LCG' % self.version.underscored, 'include')
-    # os.symlink('CORAL_%s/%s/lib' % (self.version.underscored, self.scram_arch), 'lib')
+    # os.symlink('CORAL_%s/%s/lib' % (self.version.underscored, self.cmsplatf), 'lib')
