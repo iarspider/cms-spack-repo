b2b40f8379fb5ceca361413f6e363ffec12ddf6f
iarspider <iarspider@gmail.com>
2021-07-02 17:11:27 +0200

    Correct mirror URL; re-enable binary cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,22 +14,11 @@ echo Add signing key
 spack gpg trust $SPACK_GPG_KEY
 echo Start the installation
 spack env activate CMSSW_12_0_X
-# TODO: re-enable cache
-spack install --no-cache -j$CORES --fail-fast
+spack install -j$CORES --fail-fast
 echo Prepare mirror and buildcache
 spack mirror create -d $WORKSPACE/mirror --all --dependencies
 spack buildcache create -r -f -a -d $WORKSPACE/mirror
 cd $WORKSPACE
-#export EOS_MGM_URL=root://eosuser.cern.ch
-#mirrordate=$(date +%s%N)
-#mirrorfile=mirror-${mirrordate}.tar
-#tar -cf ../$mirrorfile *
-#echo Transfer mirror
-#xrdcp $mirrorfile root://eosuser.cern.ch//eos/user/r/razumov/CMS
-#echo Unpack mirror
-#xrdfs root://eosuser.cern.ch mkdir /eos/user/r/razumov/CMS/stage-$mirrordate
-#ssh lxplus.cern.ch tar -C /eos/user/r/razumov/CMS/stage-$mirrordate -xf /eos/user/r/razumov/CMS/$mirrorfile
-#ssh lxplus.cern.ch rsync --recursive --links --ignore-times --ignore-existing --size-only /eos/user/r/razumov/www/CMS/stage-$mirrordate/ /eos/user/r/razumov/CMS/mirror
 echo Upload mirror
 rsync -e "ssh -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes" --recursive --links --ignore-existing mirror/ cmsbuild@lxplus:/eos/user/r/razumov/www/CMS/mirror
 echo Done
