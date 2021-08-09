b179ef98e7f3183deade390f4bbf2cbd10a62fbe
iarspider <iarspider@gmail.com>
2021-08-09 13:14:03 +0200

    Add missing changes in tensorflow and update environment

--- packages/py-tensorflow/package.py
+++ packages/py-tensorflow/package.py
@@ -108,10 +108,10 @@ class PyTensorflow(Package, CudaPackage):
     depends_on('py-enum34@1.1.6:', type=('build', 'run'), when='@1.5: ^python@:3.3')
     depends_on('py-enum34@1.1.6:', type=('build', 'run'), when='@1.4.0:1.4.1')
 
-    depends_on('py-gast@0.4.0', type=('build', 'run'), when='@2.5:')
-    depends_on('py-gast@0.3.3', type=('build', 'run'), when='@2.2:2.4')
-    depends_on('py-gast@0.2.2', type=('build', 'run'), when='@1.15:2.1')
-    depends_on('py-gast@0.2.0:', type=('build', 'run'), when='@1.6:1.14')
+    depends_on('py-gast@0.4.0:', type=('build', 'run'), when='@2.5.0.cms')
+    #depends_on('py-gast@0.3.3', type=('build', 'run'), when='@2.2:2.4')
+    #depends_on('py-gast@0.2.2', type=('build', 'run'), when='@1.15:2.1')
+    #depends_on('py-gast@0.2.0:', type=('build', 'run'), when='@1.6:1.14')
 
     depends_on('py-google-pasta@0.2:0.999', type=('build', 'run'), when='@2.4.0:')
     depends_on('py-google-pasta@0.1.8:', type=('build', 'run'), when='@2.1:2.3')
