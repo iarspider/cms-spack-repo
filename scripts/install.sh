61add4d5a78b16ddb6b31b0000aad076db93ea35
iarspider <iarspider@gmail.com>
2021-08-20 18:50:25 +0200

    Fix environment name

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ echo Set install root
 mkdir -p $WORKSPACE/install
 spack config add "config:install_tree:root:$WORKSPACE/install"
 echo Start the installation
-spack env activate CMSSW_12_0_X
+spack env activate CMSSW_12_1_X
 spack install -j$CORES --fail-fast --cache-only
 spack load root
 # Tests
