27f5885924776105f8ed05e1b4301b530eb08788
iarspider <iarspider@gmail.com>
2021-08-25 09:57:54 +0200

    Add hack to force correct permissions

---
+++ hook/fix_permissions.py
@@ -0,0 +1,70 @@
+import functools
+import operator
+import os
+import spack.package_prefs as prefs 
+
+from llnl.util.filesystem import join_path, force_remove
+
+
+chmod_regex = re.compile(r"(?P<who>[uoga]?)(?P<op>[+\-=])(?P<value>[ugo]|[rwx]*)")
+stat_bit_prefix = dict(u="USR", g="GRP", o="OTH")
+
+
+def chmod(entry, perms, recursive=False):
+    """Implement the bash chown function
+    Arguments:
+        entry (str): path to change
+        perms (str or int): permissions. Can be either a number,
+            or a symbolic description
+        recursive (bool): if chmod should be performed recursively
+    The format of description is
+        * an optional letter in o, g, u, a (no letter means a)
+        * an operator in +, -, =
+        * a sequence of letters in r, w, x, or a single letter in o, g, u
+    Notice: [+-]X is not supported
+    """
+    if not os.path.isdir(entry):
+        recursive = False
+
+    chmod_f = _chmod_one_s if isinstance(perms, str) else _chmod_one_o
+
+    chmod_f(entry, perms)
+    if recursive:
+        for root, dirs, files in os.walk(entry):
+            for entry in itertools.chain(dirs, files):
+                chmod_f(entry, perms)
+
+
+def _chmod_one_o(entry, perms):
+    os.chmod(entry, perms)
+
+
+def _chmod_one_s(entry, perms):
+    # Adapted from here: https://www.daniweb.com/programming/software-development/code/243659/change-file-permissions-symbolically-linux
+    def stat_bit(who, letter):
+        if who == "a":
+            return stat_bit("o", letter) | stat_bit("g", letter) | stat_bit("u", letter)
+        return getattr(stat, "S_I%s%s" % (letter.upper(), stat_bit_prefix[who]))
+
+    def ors(sequence, initial=0):
+        return functools.reduce(operator.__or__, sequence, initial)
+
+    mo = chmod_regex.match(perms)
+    who, op, value = mo.group("who"), mo.group("op"), mo.group("value")
+    if not who:
+        who = "a"
+    mode = os.stat(entry)[stat.ST_MODE]
+    if value in ("o", "g", "u"):
+        mask = ors((stat_bit(who, z) for z in "rwx" if (mode & stat_bit(value, z))))
+    else:
+        mask = ors((stat_bit(who, z) for z in value))
+    if op == "=":
+        mode &= ~ ors((stat_bit(who, z) for z in "rwx"))
+    mode = (mode & ~mask) if (op == "-") else (mode | mask)
+    os.chmod(entry, mode)
+
+
+def post_install(spec): 
+    prefix = spec.prefix
+    chmod(str(prefix), 'o+r', True)
+    chmod(str(prefix.bin), 'a+x', True)
