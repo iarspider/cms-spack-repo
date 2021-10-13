0ca3422dac62ae01a860ff10daac1cd62398452d
iarspider <iarspider@gmail.com>
2021-10-13 14:18:05 +0200

    Fix style

--- packages/alpgen/package.py
+++ packages/alpgen/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import os
+
 from spack import *
 
 
@@ -13,32 +14,33 @@ class Alpgen(CMakePackage):
 
     homepage = "http://mlm.home.cern.ch/mlm/alpgen/"
     url      = "https://mlm.home.cern.ch/mlm/alpgen/V2.1/v214.tgz"
-    
+
     patch('alpgen-214.patch', when='recipe=cms')
     patch('alpgen-214-Darwin-x86_84-gfortran.patch', when='platform=darwin recipe=cms')
     patch('alpgen-2.1.4-sft.patch', when='recipe=sft', level=0)
-    
+
     variant('recipe', values=('cms', 'sft'), default='sft',
-            description='Select build recipe: CMS for CMS experiment, '+
+            description='Select build recipe: CMS for CMS experiment, ' +
                         'SFT for ATLAS/LHCb/others.')
 
     version('2.1.4', sha256='2f43f7f526793fe5f81a3a3e1adeffe21b653a7f5851efc599ed69ea13985c5e')
-    
+
     def url_for_version(self, version):
         root = url.rsplit('/', 2)[0]
         return "{0}/V{1}/{2}.tgz".format(root, version.upto(2), version.joined)
 
     def patch(self):
         if self.spec.satisfies('recipe=sft'):
-            copy(join_path(os.path.dirname(__file__), 'CMakeLists.txt'), 'CMakeLists.txt')
-            
+            copy(join_path(os.path.dirname(__file__), 'CMakeLists.txt'), 
+                 'CMakeLists.txt')
+
         if self.spec.satisfies('recipe=cms'):
             filter_file('-fno-automatic', '-fno-automatic -std=legacy', 'compile.mk')
 
     @when('recipe=cms')
     def cmake(self, spec, prefix):
         return
-        
+
     @when('recipe=cms')
     def build(self, spec, prefix):
         build_sh = Executable('cms_build.sh')
