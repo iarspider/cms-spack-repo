ac86060eb5769e046cd9f6b602508ac025399f24
iarspider <iarspider@gmail.com>
2021-10-13 09:54:11 +0200

    Start updating toolfile packages

--- packages/coral-tool-conf/package.py
+++ packages/coral-tool-conf/package.py
@@ -9,33 +9,32 @@ class CoralToolConf(Package):
     url = 'file://' + os.path.dirname(__file__) + '/../ToolfilePackage/junk.xml' 
     version('1.0', '68841b7dcbd130afd7d236afe8fd5b949f017615', expand=False)
 
-    depends_on('scram')
-    depends_on('gcc-compiler-toolfile')
-    depends_on('systemtools')
-    depends_on('gmake-toolfile')
-    depends_on('pcre-toolfile')
-    depends_on('python-toolfile')
-    depends_on('expat-toolfile')
-    depends_on('boost-toolfile')
-    depends_on('frontier-client-toolfile')
-    depends_on('openssl-toolfile')
-
-
-    depends_on('sqlite-toolfile')
-    depends_on('uuid-toolfile')
-    depends_on('zlib-toolfile')
-    depends_on('bzip2-toolfile')
-    depends_on('cppunit-toolfile')
-    depends_on('xerces-c-toolfile')
-    depends_on('oracle-toolfile')
-    depends_on('oracleocci-abi-hack-cms-toolfile')
-
+    depends_on('pcre')
+    depends_on('python')
+    depends_on('expat')
+    depends_on('boost')
+    depends_on('frontier-client')
+    depends_on('sqlite')
+    depends_on('util-linux-uuid')
+    depends_on('zlib')
+    depends_on('bzip2')
+    depends_on('xerces-c')
+    depends_on('oracle-instant-client', when='arch=amd64')
 
     def install(self, spec, prefix):
+        get_tools_path = join_path(os.path.dirname(__file__), '..', 'ToolfilePackage', 'bin', 'get_tools')
+        set_executable(get_tools_path)
+        get_tools = Executable(get_tools_path)
+        
         with working_dir(prefix, create=True):
             mkdirp('tools/selected')
             mkdirp('tools/available')
             for dep in spec.dependencies():
-                xmlfiles = glob(join_path(dep.prefix.etc, 'scram.d', '*.xml'))
-                for xmlfile in xmlfiles:
-                    install(xmlfile, 'tools/selected')
+                uctool = dep.name.upper().replace('-', '_')
+                toolbase = dep.prefix
+                toolver = dep.version
+                
+                get_tools(toolbase, toolver, self.prefix, dep.name)
+
+        get_tools("", "system", self.prefix, "systemtools")
+        # TODO: vectorization
