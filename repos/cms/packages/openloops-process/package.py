90b38f3ffc17b32ecb81bf8455675de596ada13c
iarspider <iarspider@gmail.com>
2022-02-04 11:26:41 +0100

    Add 'tiny' variant for openloops (for debug purposes only\!)

--- repos/cms/packages/openloops-process/package.py
+++ repos/cms/packages/openloops-process/package.py
@@ -16,15 +16,18 @@ class OpenloopsProcess(Package):
     version('2.1.2', branch='cms/v2.1.2')
     patch('openloops-urlopen2curl.patch')
 
+    variant('tiny', default=False, description='Only download one process to speed things up')
+
     depends_on('python@2.7,3.2:', type='build')
     
     def install(self, spec, prefix):
+        coll_file = 'cms.coll' if not self.spec.variants['tiny'].value else 'tiny.coll'
         if self.spec.satisfies('target=aarch64:'):
-            filter_file('pplljj_ew', '', 'cms.coll')
+            filter_file('pplljj_ew', '', coll_file)
 
-        copy(join_path(os.path.dirname(__file__), 'cms.coll'), 'cms.coll') 
+        copy(join_path(os.path.dirname(__file__), coll_file), coll_file) 
         downloader = Executable('./pyol/bin/download_process.py')
-        downloader('cms.coll')
+        downloader(coll_file)
         install_tree('process_src', self.prefix.process_src)
         install_tree('proclib', self.prefix.proclib)
-        install('cms.coll', self.prefix)
+        install(coll_file, self.prefix)
