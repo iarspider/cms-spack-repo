fe29e416252b95dfcb3dd3bc0b335b781a064f7f
iarspider <iarspider@gmail.com>
2022-02-17 09:40:21 +0100

    Fix cmspost scripts for crab-x

--- repos/cms/packages/crab-prod/cmspost.sh
+++ repos/cms/packages/crab-prod/cmspost.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+pkgrel=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${RPM_INSTALL_PREFIX}
 mkdir -p share/cms/crab-prod/%{ver}
 for dir in bin lib etc examples; do
-  rsync -a ${dir}/ share/cms/crab-prod/%{ver}/${dir}/
+  rsync -a ${pkgrel}/${dir}/ share/cms/crab-prod/%{ver}/${dir}/
 done
