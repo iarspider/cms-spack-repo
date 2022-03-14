3ca8bd50745ab8303dd04701a4c0113a715961d7
iarspider <iarspider@gmail.com>
2022-03-14 15:29:21 +0100

    Update backport script

--- scripts/backport.sh
+++ scripts/backport.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
-vanilla_root="/home/razumov/Work/_CMS/vanilla_spack/var/spack/repos/builtin/packages"
+upstream="/home/razumov/Work/_CMS/vanilla_spack/var/spack/repos/builtin/packages"
+#########################################################################################################################
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $(dirname $SCRIPT_DIR)
 [ $# -lt 1 ] && (echo "Usage: backport.sh <package> [<package> ...]"; exit 1)
 for pname in "$@"
 do
-    [ ! -d ${vanilla_root}/${pname} -o ! -f ${vanilla_root}/${pname}/package.py ] && (echo "Can't find recipe for $pname"; exit 2)
-    cp -rf ${vanilla_root}/${pname} repos/backport/packages
+    [ ! -d ${upstream}/${pname} -o ! -f ${upstream}/${pname}/package.py ] && (echo "Can't find recipe for $pname"; exit 2)
+    cp -rf ${upstream}/${pname} repos/backport/packages
     cp -rf repos/backport/packages/${pname} spack/var/spack/repos/builtin/packages
 done
