1036a60d1db9a220490cb51571e4b6b5d2bfdf1b
iarspider <iarspider@gmail.com>
2022-03-22 22:55:11 +0100

    Do all the patching earlier

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,16 +27,16 @@ echo "from spack.build_systems.crab import CrabPackage" >> lib/spack/spack/pkgki
 echo Adding CMSData package type
 cp ${SCRIPT_DIR}/build_systems/cmsdata.py lib/spack/spack/build_systems/
 echo "from spack.build_systems.cmsdata import CMSDataPackage" >> lib/spack/spack/pkgkit.py
-echo Initializing Spack
-source share/spack/setup-env.sh
-echo Adding CMS repository
-spack repo add --scope=site ${SCRIPT_DIR}/repos/cms
 echo Copying backported recipes
 ##spack repo add --scope=site ${SCRIPT_DIR}/repos/backport
 find ${SCRIPT_DIR}/repos/backport/packages -maxdepth 1 -type 'd' -exec cp -r -f {} ${SCRIPT_DIR}/spack/var/spack/repos/builtin/packages \;
 echo Copying backported PythonPackage class
 cp ${SCRIPT_DIR}/build_systems/python.py lib/spack/spack/build_systems/
 cp ${SCRIPT_DIR}/develop/build_environment.py lib/spack/spack/build_environment.py
+echo Initializing Spack
+source share/spack/setup-env.sh
+echo Adding CMS repository
+spack repo add --scope=site ${SCRIPT_DIR}/repos/cms
 echo Adding CMS mirror
 spack mirror add --scope=site cms https://test-cms-spack.web.cern.ch/test-cms-spack/CMS/mirror
 echo Creating environment
