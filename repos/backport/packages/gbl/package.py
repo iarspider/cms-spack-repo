8a4ef9703e866564c319e2cecb11cef2da471100
iarspider <iarspider@gmail.com>
2022-04-01 14:57:57 +0200

    Add elfutils; move gbl to backports

--- repos/backport/packages/gbl/package.py
+++ repos/backport/packages/gbl/package.py
@@ -12,13 +12,19 @@ class Gbl(CMakePackage):
     homepage = "https://www.desy.de/~kleinwrt/GBL/doc/cpp/html/"
     git      = "https://gitlab.desy.de/claus.kleinwort/general-broken-lines.git"
 
-    version('V02-01-03', tag="V02-01-03")
+    maintainers = ['iarspider']
+    tags = ['hep']
 
+    version('V02-04-01', commit="1061b643c6656fbf7ceba579997eb43f0a9e9d3c")
+    version('V02-01-03', commit="8acaade19c20e9ef23d1244a555fead6ef149c33")
+
+    variant('root', default=True, description='Support ROOT for user I/O')
     depends_on('eigen', type=('build', 'link'))
+    depends_on('root', type=('build', 'link'), when='+root')
 
     root_cmakelists_dir = 'cpp'
 
     def cmake_args(self):
         args = [self.define('EIGEN3_INCLUDE_DIR', self.spec['eigen'].prefix.include.eigen3),
-                self.define('SUPPORT_ROOT', False)]
+                self.define_from_variant('SUPPORT_ROOT', 'root')]
         return args
