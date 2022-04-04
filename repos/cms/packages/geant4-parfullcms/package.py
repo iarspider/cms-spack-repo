e324fea5c3188f0b7b15321c24d44ae79bbda7d1
iarspider <iarspider@gmail.com>
2022-04-04 16:40:51 +0200

    New pacakage: geant4-parfullcms

---
+++ repos/cms/packages/geant4-parfullcms/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Geant4Parfullcms(CMakePackage):
+    """CPU benchmark test of Geant4 based on the full CMS detector"""
+
+    homepage = "https://github.com/cms-sw/cmssw"
+    url      = "https://cmsrep.cern.ch/cmssw/repos/cms/SOURCES/slc7_amd64_gcc10/external/geant4-parfullcms/2014.01.27/ParFullCMS.2014.01.27.tar.bz2"
+
+    version('2014.01.27', sha256='96680b6db41951515dc4cce921b5857467543631775f2eeacff3880d0481129f')
+
+    depends_on('geant4')
+    depends_on('geant4data')
+
+    def cmake_args(self):
+        args = []
+        args += self.define('BUILD_SHARED_LIBS', False)
+        args += self.define('BUILD_STATIC_LIBS', True)
+        args += self.define('Geant4_USE_FILE', self.spec['geant4'].prefix)
+        return args
