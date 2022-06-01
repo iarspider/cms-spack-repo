e586cac0980f4178591ddee71db3a93e68536ac4
iarspider <iarspider@gmail.com>
2022-06-01 22:45:03 +0200

    Fix

--- repos/cms/packages/cmssw-osenv/package.py
+++ repos/cms/packages/cmssw-osenv/package.py
@@ -17,7 +17,7 @@ class CmsswOsenv(Package):
 
     def install(self, spec, prefix):
         mkdirp(prefix.common)
-        shutil.rmtree('.git')
+        # shutil.rmtree('.git')
         install_tree('.', prefix.common)
         install(join_path(os.path.dirname(__file__), 'cmspost.sh'), prefix)
         filter_file('^pkgname=.*', 'pkgname="'+os.path.basename(os.path.dirname(__file__))+'"', prefix.join('cmspost.sh'), backup=False, stop_at='## END CONFIG')
