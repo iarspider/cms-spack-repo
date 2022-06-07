3b604b6da643824574db8d133f1fe602b5eea4ff
iarspider <iarspider@gmail.com>
2022-06-07 17:14:20 +0200

    Fix install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,15 @@
 # if [ -z ${RPM_INSTALL_PREFIX+x} ]; then export RPM_INSTALL_PREFIX=/cvmfs/cms-ib.cern.ch/spack; fi
 if [[ "${RPM_INSTALL_PREFIX}" != /* ]]; then RPM_INSTALL_PREFIX=${WORKSPACE}/${RPM_INSTALL_PREFIX}; fi
 
-if [ `uname` == "Darwin" ]; then
-	CORES=`sysctl -n hw.ncpu`
-elif [ `uname` == "Linux" ]; then
-	CORES=`awk '/^processor/ { N++} END { print N }' /proc/cpuinfo`
+if [ "$(uname)" == "Darwin" ]; then
+	CORES=$(sysctl -n hw.ncpu)
+elif [ "$(uname)" == "Linux" ]; then
+	CORES=$(awk '/^processor/ { N++} END { print N }' /proc/cpuinfo)
 fi
 export CORES
 echo Setup Spack for CMS
-cd $WORKSPACE/cms-spack-repo
-bash -xe ./bootstrap.sh
+cd "$WORKSPACE"/cms-spack-repo
+bash -xe ./bootstrap.sh || (echo "Boostrap failed"; exit 1)
 cd spack
 export SPACK_DISABLE_LOCAL_CONFIG=true
 export SPACK_USER_CACHE_PATH=$WORKSPACE
@@ -20,12 +20,11 @@ echo Add signing key
 bin/spack buildcache keys --force --install --trust
 echo Set install root
 bin/spack config add "config:install_tree:root:${RPM_INSTALL_PREFIX}"
-mkdir -p ${RPM_INSTALL_PREFIX}
 echo Start the installation
 if [[ "${RPM_INSTALL_PREFIX}" == /cvmfs* ]]; then cvmfs_server transaction cms-ib.cern.ch; fi
+mkdir -p "${RPM_INSTALL_PREFIX}"
 #spack env activate ${SPACK_ENV_NAME}
-# CMS post-install
-bin/spack -e ${SPACK_ENV_NAME} install -j$CORES --fail-fast --cache-only
+bin/spack -e "${SPACK_ENV_NAME}" install -j"$CORES" --fail-fast --cache-only --require-full-hash-match
 if [$? -eq 0 ]; then
     echo Installation complete
     if [[ "${RPM_INSTALL_PREFIX}" == /cvmfs* ]]; then cvmfs_server publish cms-ib.cern.ch; fi
@@ -34,4 +33,4 @@ else
     if [[ "${RPM_INSTALL_PREFIX}" == /cvmfs* ]]; then cvmfs_server abort cms-ib.cern.ch; fi
 fi
 # Simple test
-bin/spack -e ${SPACK_ENV_NAME} find -p cmssw
+bin/spack -e "${SPACK_ENV_NAME}" find -p cmssw
