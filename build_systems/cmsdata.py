bb219c9aba10e6177ac47106e2aa536c15b441c6
Ivan Razumov <iarspider@gmail.com>
2022-06-17 17:17:31 +0200

    Directly install all data- packages under share

--- build_systems/cmsdata.py
+++ build_systems/cmsdata.py
@@ -16,19 +16,7 @@ class CMSDataPackage(PackageBase):
         data_dir = getattr(self, 'data_dir', None)
         data_dir = data_dir or join_path(data_repo.replace('-', '/'), data)
 
-        if os.environ.get('RPM_INSTALL_PREFIX', None):
-            install_root = join_path(os.environ.get('RPM_INSTALL_PREFIX'),
-                                     'share', 'cms', n, str(spec.version))
-            if os.path.exists(join_path(install_root, data_dir)):
-                touch(join_path(prefix, '.already_installed'))
-                return
-        else:
-            install_root = prefix
+        install_root = prefix
 
         mkdirp(join_path(install_root, data_dir))
         install_tree('.', join_path(install_root, data_dir))
-        if os.environ.get('RPM_INSTALL_PREFIX', None):
-            d = data_repo.split('-', 1)[0]
-            symlink(join_path(os.environ.get('RPM_INSTALL_PREFIX'),
-                              'share', 'cms', n, str(spec.version), d),
-                    join_path(prefix, d))
