46846986c8c6a6eae58fc12762ab0acd0858c57f
Ivan Razumov <ivan.razumov@cern.ch>
2022-06-24 11:19:45 +0200

    Fixes

--- repos/cms/packages/cudnn/package.py
+++ repos/cms/packages/cudnn/package.py
@@ -13,5 +13,9 @@ class Cudnn(BuiltinCudnn):
         for fn in glob.glob(prefix.lib.join('*.a')):
             os.unlink(fn)
 
+        # -- CMS: remove static libraries
+        for fn in glob.glob(prefix.lib.join('*.a')):
+            os.unlink(fn)
+
         # -- CMS: onnxruntime is hardcoded to look for the cudnn libraries under .../lib64
         shutil.move(prefix.lib, prefix.lib64)
