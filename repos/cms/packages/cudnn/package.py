51759df63a36b8e07b6ae6130ba3ed7e4f564df2
Ivan Razumov <iarspider@gmail.com>
2022-06-27 15:54:19 +0200

    Add backported sherpa recipe

--- repos/cms/packages/cudnn/package.py
+++ repos/cms/packages/cudnn/package.py
@@ -1,3 +1,5 @@
+import shutil
+
 from spack import *
 from spack.pkg.builtin.cudnn import Cudnn as BuiltinCudnn
 
@@ -7,5 +9,9 @@ class Cudnn(BuiltinCudnn):
 
     @run_after('install')
     def post_install(self):
+        # -- CMS: remove static libraries
+        for fn in glob.glob(prefix.lib.join('*.a')):
+            os.unlink(fn)
+
         # -- CMS: onnxruntime is hardcoded to look for the cudnn libraries under .../lib64
         shutil.move(prefix.lib, prefix.lib64)
