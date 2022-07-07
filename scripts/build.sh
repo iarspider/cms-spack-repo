b28b9408bb5fe72ba2f2daecb8dbbee9f72523a0
Ivan Razumov <ivan.razumov@cern.ch>
2022-07-07 16:10:10 +0200

    cmsset_default works now! Yay!

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,6 +30,10 @@ function retry {
   echo $output
 }
 
+[ -z ${WORKSPACE+x} ] && (echo 'ERROR: WORKSPACE not set, quitting'; exit 1)
+[ -z ${RPM_INSTALL_PREFIX+x} ] && (echo 'ERROR: RPM_INSTALL_PREFIX not set, quitting'; exit 2)
+[ -z ${SPACK_ENV_NAME+x} ] && (echo 'ERROR: SPACK_ENV_NAME not set, quitting'; exit 3)
+
 if [ `uname` == "Darwin" ]; then
     CORES=`sysctl -n hw.ncpu`
 elif [ `uname` == "Linux" ]; then
