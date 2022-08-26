a21adf0932701c20366399e2a4e58e3448b37023
Ivan Razumov <ivan.razumov@cern.ch>
2022-08-26 14:43:02 +0200

    Try forcing use of gcc10 for bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,9 @@ cp ${WORKSPACE}/cms-spack-repo/config/config.yaml etc/spack/
 echo Adding external gcc
 mkdir -p etc/spack/linux
 cp ${WORKSPACE}/cms-spack-repo/config/compilers.yaml etc/spack/linux/compilers.yaml
+echo Forcing use of external gcc for bootstrapping
+mkdir -p ${WORKSPACE}/bootstrap/config/linux
+cp ${WORKSPACE}/cms-spack-repo/config/compilers.yaml ${WORKSPACE}/bootstrap/config/linux/
 echo Adding CMS hooks
 cp ${WORKSPACE}/cms-spack-repo/hook/* lib/spack/spack/hooks/
 echo Adding SCRAM build system support
