bd28b3194bd093665d12e7cf9338cd7bab919cb3
Andrea Valenzuela <aand.valenzuela@gmail.com>
2022-09-20 17:42:36 +0200

    Rename ittnotify patch

--- repos/cms/packages/ittnotify/package.py
+++ repos/cms/packages/ittnotify/package.py
@@ -17,7 +17,7 @@ class Ittnotify(CMakePackage):
 
     keep_archives = True
 
-    patch("aarch64-ppc64le.patch", when="target=aarch64:")
+    patch("aarch64.patch", when="target=aarch64:")
 
     def cmake_args(self):
         args = [self.define('ARCH_64', 1)]
