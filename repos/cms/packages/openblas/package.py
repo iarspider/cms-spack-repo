59680eaec7b0045caba12cfb878b8165fd9e01a1
Cms Build <cmsbuild@cern.ch>
2022-09-20 09:23:08 +0200

    Fix openblas CFLAGS

--- repos/cms/packages/openblas/package.py
+++ repos/cms/packages/openblas/package.py
@@ -270,11 +270,8 @@ class Openblas(MakefilePackage):
         elif self.spec.target.family == 'aarch64':
             make_defs.append('TARGET=ARMV8')
         elif self.spec.target.family == 'ppc64le':
-            make_defs.append('CFLAGS="-mlong-double-64 '
-                              '-mcpu=power8 -mtune=power8 ',
-                              '--param=l1-cache-size=64 ',
-                              '--param=l1-cache-line-size=128 ',
-                              '--param=l2-cache-size=51"')
+            make_defs.extend(['BINARY=64', 'NUM_THREADS=256', 'DYNAMIC_ARCH=0',
+                              'CFLAGS=-mcpu=power8 -mtune=power8 --param=l1-cache-size=64 --param=l1-cache-line-size=128 --param=l2-cache-size=512'])
         else:
             # -- CMS: Fallback to Spack behaviour
             make_defs += self._microarch_target_args()
