d0605ec1f167431593a4fee67cc0fc5e991b36cb
Ivan Razumov <ivan.razumov@cern.ch>
2022-09-26 11:45:44 +0200

    Update scripts: copy and retrieve lock file

--- scripts/singBuild.sh
+++ scripts/singBuild.sh
@@ -24,12 +24,27 @@ rm -f ${WORKSPACE}/fail
 [ ! -e ${WORKSPACE}/spack ] && bash -xe ${WORKSPACE}/cms-spack-repo/bootstrap.sh
 
 ${WORKSPACE}/cms-bot/docker_launcher.sh ${WORKSPACE}/cms-spack-repo/scripts/build.sh
-if [ -e ${WORKSPACE}/fail ]; then
-#    echo Build falied, uploading monitor data
-#    tar -zcf ${WORKSPACE}/monitor.tar.gz ${WORKSPACE}/monitor
-#    scp ${WORKSPACE}/monitor.tar.gz cmsbuild@lxplus:/eos/user/r/razumov/www/CMS/mirror
-#    rm ${WORKSPACE}/monitor.tar.gz
-    touch ${WORKSPACE}/fail
-    exit 1
+if [ ${exit_code} -ne 0 ]; then
+    echo Build failed, uploading logs
+    ssh $SSH_OPTS lxplus rm -rf /eos/user/r/razumov/www/CMS/logs/${SPACK_ENV_NAME}-${SCRAM_ARCH}
+    ssh $SSH_OPTS lxplus mkdir /eos/user/r/razumov/www/CMS/logs/${SPACK_ENV_NAME}-${SCRAM_ARCH}
+    pushd ${WORKSPACE}/spack/stage
+    find . -maxdepth 1 -type d -name 'spack-stage-*' -print0 | while read -d $'\0' dirn
+    do
+        ssh $SSH_OPTS lxplus mkdir /eos/user/r/razumov/www/CMS/logs/${SPACK_ENV_NAME}-${SCRAM_ARCH}/$dirn
+        scp $SSH_OPTS $d/*.txt lxplus:/eos/user/r/razumov/www/CMS/logs/${SPACK_ENV_NAME}-${SCRAM_ARCH}/$dirn
+    done
+    popd
+    scp $SSH_OPTS ${WORKSPACE}/spack/var/spack/environments/${SPACK_ENV_NAME}/spack.lock lxplus:/eos/user/r/razumov/www/CMS/logs/${SPACK_ENV_NAME}-${SCRAM_ARCH}/
+    touch $WORKSPACE/fail
+    exit ${exit_code}
 fi
-echo All done
+
+echo Create and upload buildcache
+if [ ${UPLOAD_BUILDCACHE-x} = "true" ]; then
+  echo Prepare mirror and buildcache
+  # TODO: push gpg key to mirror (broken in 0.17, should be working in 0.18)
+  ${WORKSPACE}/spack/bin/spack -e ${SPACK_ENV_NAME} buildcache create -r -a -f --mirror-url s3://cms-spack/${SCRAM_ARCH}/
+  scp ${WORKSPACE}/spack/var/spack/environments/${SPACK_ENV_NAME}/spack.lock lxplus:/eos/user/r/razumov/www/CMS/environment/spack-${SPACK_ENV_NAME}-${SCRAM_ARCH}.lock
+fi
+echo build.sh done
