3ebdd240adc515e75dd0dbef01e35836e823e78d
Ivan Razumov <iarspider@gmail.com>
2022-10-13 15:26:43 +0200

    New version of augmnent command

--- spack-scripting/scripting/cmd/augment.py
+++ spack-scripting/scripting/cmd/augment.py
@@ -50,7 +50,7 @@ def augment(parser, args):
         mkdirp(os.path.dirname(target_path))
         with open(target_path, "w") as pkg_file:
             pkg_file.write(PACKAGE_TEMPLATE.format(
-                module=spec.package.fullname.replace('-', '_'),
+                module=spec.package_class.fullname.replace('-', '_'),
                 namespace=repo.namespace.capitalize(),
                 cls=mod_to_class(name)))
     if args.split:
@@ -60,8 +60,47 @@ def augment(parser, args):
 
 
 PACKAGE_TEMPLATE = """\
+import copy
+
 from spack import *
 from spack.pkg.{module} import {cls} as {namespace}{cls}
+
+
+def drop_patch(cls, fn):
+    def filter_func(p):
+        if isinstance(p, FilePatch):
+            return p.relative_path != fn
+        elif isinstance(p, UrlPatch):
+            return p.url != fn
+
+    old_patches = copy.deepcopy(cls.patches)
+    new_patches = {{}}
+    for spec, patches in old_patches.items():
+        filtered_patches = [p for p in patches if filter_func(p)]
+        if filtered_patches:
+            new_patches[spec] = filtered_patches
+
+    cls.patches = new_patches
+    del old_patches
+    del new_patches
+
+
+def drop_dependency(cls, name):
+    dependencies = {{}}
+    for k, v in cls.dependencies.items():
+        if k != name:
+            dependencies[k] = v
+
+    cls.dependencies = dependencies
+
+
+def drop_conflicts(cls, name):
+    conflicts = {{}}
+    for k, v in cls.conflicts.items():
+        if k != name:
+            conflicts[k] = v
+
+    cls.conflicts = conflicts
 
 
 class {cls}({namespace}{cls}):
