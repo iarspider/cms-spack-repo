06f31bbfe41569b344d5a14354feef3f25794156
Ivan Razumov <iarspider@gmail.com>
2022-11-18 14:42:04 +0100

    Fix

--- build_systems/scramtoolfile.py
+++ build_systems/scramtoolfile.py
@@ -145,8 +145,8 @@ class ScramToolfilePackage(BundlePackage):
                 f.write(pkg + "\n")
 
         with open(join_path(prefix.tools.selected, "python-paths.xml"), "a") as f:
-            f.write("<tool name="python-paths" version="1.0">\n")
-            f.write("  <runtime name="PYTHON3PATH"  value="" + join_path(prefix, self.site_packages_dir, "site-packages")  + "" type="path"/>\n")
+            f.write("<tool name="\python-paths\" version=\"1.0\">\n")
+            f.write("  <runtime name=\"PYTHON3PATH\"  value=\"" + join_path(prefix, self.site_packages_dir, "site-packages")  + "\" type=\"path\"/>\n")
             f.write("</tool>\n")
 
 
