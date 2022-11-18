de483cd456a6297efcd70c41015d00f1f958d49e
Ivan Razumov <iarspider@gmail.com>
2022-11-18 14:34:51 +0100

    Continue updating custom build_systems

--- build_systems/cmsdata.py
+++ build_systems/cmsdata.py
@@ -1,29 +1,33 @@
-import os
-import shutil
+import llnl.util.filesystem as fs
 
-from llnl.util.filesystem import *
-
+import spack.builder
 import spack.package_base
+from spack.directives import build_system, conflicts
+
+from ._checks import (
+    BaseBuilder,
+    apply_macos_rpath_fixups,
+)
+
 
 class CMSDataPackage(spack.package_base.PackageBase):
-    build_system_class = 'CMSDataPackage'
-    phases = ['install']
+    build_system_class = "CMSDataPackage"
+    build_system("cmsdata")
+
+               
+@spack.builder.builder("cmsdata")
+class CMSDataBuilder(BaseBuilder):
+    phases = ("install",)
 
     def install(self, spec, prefix):
-        data = getattr(self, 'data', None) or "data"
+        data = getattr(self, "data", None) or "data"
         n = self.n
-        data_repo = getattr(self, 'data_repo', n.replace('data-', ''))
-        data_dir = getattr(self, 'data_dir', None)
-        data_dir = data_dir or join_path(data_repo.replace('-', '/'), data)
+        data_repo = getattr(self, "data_repo", n.replace("data-", ""))
+        data_dir = getattr(self, "data_dir", None)
+        data_dir = data_dir or fs.join_path(data_repo.replace("-", "/"), data)
 
         install_root = prefix
 
         mkdirp(join_path(install_root, data_dir))
-        install_tree('.', join_path(install_root, data_dir))
-        gitdirs = []
-        for fn in find(install_root, '.git'):
-            gitdirs.append(fn)
+        install_tree(".", fs.join_path(install_root, data_dir), ignore=lambda x: ".git" in x)
 
-        for fn in gitdirs:
-            if os.path.isdir(fn):
-                shutil.rmtree(fn)
