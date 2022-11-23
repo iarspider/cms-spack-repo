d91926562e9eacc618124678869476fdec20e83d
Ivan Razumov <iarspider@gmail.com>
2022-11-23 15:36:46 +0100

    Update buildsystems

--- build_systems/cmsdata.py
+++ build_systems/cmsdata.py
@@ -1,30 +1,33 @@
-import os
-import shutil
+import llnl.util.filesystem as fs
 
-from llnl.util.filesystem import *
+import spack.builder
+import spack.package_base
+from spack.directives import build_system, conflicts
 
-from spack.package import PackageBase
+from ._checks import (
+    BaseBuilder,
+    apply_macos_rpath_fixups,
+)
 
 
-class CMSDataPackage(PackageBase):
-    build_system_class = 'CMSDataPackage'
-    phases = ['install']
+class CMSDataPackage(spack.package_base.PackageBase):
+    build_system_class = "CMSDataPackage"
+    build_system("cmsdata")
 
-    def install(self, spec, prefix):
-        data = getattr(self, 'data', None) or "data"
-        n = self.n
-        data_repo = getattr(self, 'data_repo', n.replace('data-', ''))
-        data_dir = getattr(self, 'data_dir', None)
-        data_dir = data_dir or join_path(data_repo.replace('-', '/'), data)
+               
+@spack.builder.builder("cmsdata")
+class CMSDataBuilder(BaseBuilder):
+    phases = ("install",)
+
+    def install(self, pkg, spec, prefix):
+        data = getattr(pkg, "data", None) or "data"
+        n = pkg.n
+        data_repo = getattr(pkg, "data_repo", n.replace("data-", ""))
+        data_dir = getattr(pkg, "data_dir", None)
+        data_dir = data_dir or fs.join_path(data_repo.replace("-", "/"), data)
 
         install_root = prefix
 
         mkdirp(join_path(install_root, data_dir))
-        install_tree('.', join_path(install_root, data_dir))
-        gitdirs = []
-        for fn in find(install_root, '.git'):
-            gitdirs.append(fn)
+        install_tree(".", fs.join_path(install_root, data_dir), ignore=lambda x: ".git" in x)
 
-        for fn in gitdirs:
-            if os.path.isdir(fn):
-                shutil.rmtree(fn)
--- build_systems/crab.py
+++ build_systems/crab.py
@@ -3,83 +3,93 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from llnl.util.filesystem import *
+import llnl.util.filesystem as fs
 
-from spack.package import PackageBase
-from spack.directives import resource, version
+import spack.builder
+import spack.package_base
+from spack.directives import build_system, conflicts, resource, version
+
+from ._checks import (
+    BaseBuilder,
+    apply_macos_rpath_fixups,
+)
 
 import glob
 import os
 import re
 
-class CrabPackage(PackageBase):
+
+class CrabPackage(spack.package_base.PackageBase):
     """crab-prod"""
 
     homepage = "https://www.example.com"
-    url = "file://" + os.path.dirname(__file__) + '/junk.xml'
+    url = "file://" + os.path.dirname(__file__) + "/junk.xml"
 
-    wmcore_version = '1.5.3'
-    crab_client_version = 'v3.220104'
-    crab_client_revision = '00'
-    crab_server_version = 'v3.220107'
-    dbs_version = '3.14.0'
+    wmcore_version = "1.5.3"
+    crab_client_version = "v3.220104"
+    crab_client_revision = "00"
+    crab_server_version = "v3.220107"
+    dbs_version = "3.14.0"
 
-    build_system_class='CrabPackage'
+    build_system_class="CrabPackage"
+    build_system("crab")
 
     resource(
-        name='CRABClient',
-        git='git://github.com/dmwm/CRABClient.git',
+        name="CRABClient",
+        git="git://github.com/dmwm/CRABClient.git",
         tag=crab_client_version,
-        destination='.'
+        destination="."
     )
     resource(
-        name='WMCore',
-        git='git://github.com/dmwm/WMCore.git',
+        name="WMCore",
+        git="git://github.com/dmwm/WMCore.git",
         tag=wmcore_version,
-        destination='.'
+        destination="."
     )
     resource(
-        name='CRABServer',
-        git='git://github.com/dmwm/CRABServer.git',
+        name="CRABServer",
+        git="git://github.com/dmwm/CRABServer.git",
         tag=crab_server_version,
-        destination='.'
+        destination="."
     )
     resource(
-        name='DBS',
-        git='git://github.com/dmwm/DBS.git',
+        name="DBS",
+        git="git://github.com/dmwm/DBS.git",
         tag=dbs_version,
-        destination='.'
+        destination="."
     )
 
-    version(crab_client_version + '.' + crab_client_revision, sha256='af147dd4a6c715dde7e0f13f4fb990029c13833d73fffc281a09709ce422c46b', expand=False)
+    version(crab_client_version + "." + crab_client_revision, sha256="af147dd4a6c715dde7e0f13f4fb990029c13833d73fffc281a09709ce422c46b", expand=False)
 
-    wmcore_packages = ("PSetTweaks", "Utils", "WMCore")
+
+@spack.builder.builder("crab")
+class CrabBuilder(BaseBuilder):
     crabserver_packages = ("RESTInteractions.py", "ServerUtilities.py")
     dbs_packages = ("Client/src/python/dbs", "PycurlClient/src/python/RestClient")
 
-    phases = ['install']
+    phases = ("install",)
 
-    def install(self, spec, prefix):
-        crab_type = os.path.basename(os.path.dirname(__file__)).replace('crab-', '')
+    def install(self, pkg, spec, prefix):
+        crab_type = os.path.basename(os.path.dirname(__file__)).replace("crab-", "")
         #Copy CRABClient
-        install_tree(join_path('CRABClient', 'src', 'python'), prefix.lib)
-        filter_file('"development"', '"{0}"'.format(self.spec.version), prefix.lib.CRABClient.join("__init__.py"))
-        install_tree(join_path('CRABClient', 'bin'), prefix.bin)
-        install_tree(join_path('CRABClient', 'etc'), prefix.etc)
+        install_tree(join_path("CRABClient", "src", "python"), prefix.lib)
+        filter_file("\"development\"", "\"{0}\"".format(spec.version), prefix.lib.CRABClient.join("__init__.py"))
+        install_tree(join_path("CRABClient", "bin"), prefix.bin)
+        install_tree(join_path("CRABClient", "etc"), prefix.etc)
 
         #List of CRAB python pakcages for which we need to create ProxyPackage symlink
-        with open(prefix.etc.join('crab_py_pkgs.txt'), 'w') as f:
-            for fn in glob.glob('CRABClient/src/python/*/__init__.py'):
-                pname = re.sub('/__init__.py$', '', fn)
-                pname = re.sub('.*/', '', pname)
+        with open(prefix.etc.join("crab_py_pkgs.txt"), "w") as f:
+            for fn in glob.glob("CRABClient/src/python/*/__init__.py"):
+                pname = re.sub("/__init__.py$", "", fn)
+                pname = re.sub(".*/", "", pname)
                 f.write(os.path.dirname(pname) + "\n")
-            f.write('dbs\n')
-            f.write('RestClient\n')
+            f.write("dbs\n")
+            f.write("RestClient\n")
 
         #Create fake WMCore
         mkdirp(prefix.lib.WMCore)
-        install('CRABClient/src/python/CRABClient/WMCoreConfiguration.py', prefix.lib.WMCore)
-        touch(prefix.lib.WMCore.join('__init__.py'))
+        install("CRABClient/src/python/CRABClient/WMCoreConfiguration.py", prefix.lib.WMCore)
+        touch(prefix.lib.WMCore.join("__init__.py"))
 
         #Copy CRABServer
         for pkg in self.crabserver_packages:
@@ -97,21 +107,26 @@ class CrabPackage(PackageBase):
         install_tree("DBS/Client/utils", prefix.examples)
 
         flags = [False, False]
-        with open(prefix.etc.join('crab-bash-completion.sh')) as f:
+        with open(prefix.etc.join("crab-bash-completion.sh")) as f:
             for line in f:
-                if '_UseCrab' in line:
+                if "_UseCrab" in line:
                     flags[0] = True
-                if re.search(' filenames  *crab', line):
+                if re.search(" filenames  *crab", line):
                     flags[1] = True
 
         if all(flags):
-            filter_file('_UseCrab', '_UseCrab_' + crab_type, prefix.etc.join('crab-bash-completion.sh'))
-            filter_file(' filenames  *crab', ' filenames crab-' + crab_type, prefix.etc.join('crab-bash-completion.sh'))
-            if crab_type == 'prod':
-                with open(prefix.etc.join('crab-bash-completion.sh'), 'a') as f:
+            filter_file("_UseCrab", "_UseCrab_" + crab_type, prefix.etc.join("crab-bash-completion.sh"))
+            filter_file(" filenames  *crab", " filenames crab-" + crab_type, prefix.etc.join("crab-bash-completion.sh"))
+            if crab_type == "prod":
+                with open(prefix.etc.join("crab-bash-completion.sh"), "a") as f:
                     f.write("complete -F _UseCrab_prod -o filenames crab\n")
         else:
             raise RuntimeError("ERROR: Unable to fix crab use function _UseCrab")
-        install(join_path(self.thisdir, 'cmspost.sh'), prefix)
-        filter_file('%{ver}', str(spec.version), join_path(prefix, 'cmspost.sh'))
-        set_executable(join_path(prefix, 'cmspost.sh'))
+
+        install(join_path(self.thisdir, "cmspost.sh"), prefix)
+        filter_file("%{ver}", str(spec.version), join_path(prefix, "cmspost.sh"))
+        set_executable(join_path(prefix, "cmspost.sh"))
+
+    # On macOS, force rpaths for shared library IDs and remove duplicate rpaths
+    spack.builder.run_after("install", when="platform=darwin")(apply_macos_rpath_fixups)
+
