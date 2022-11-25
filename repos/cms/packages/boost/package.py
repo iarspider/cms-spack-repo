4eac4a4c18c2395e28ef0c0ed4491ba5da979ebd
Ivan Razumov <ivan.razumov@cern.ch>
2022-11-25 14:27:04 +0100

    Updates

--- repos/cms/packages/boost/package.py
+++ repos/cms/packages/boost/package.py
@@ -11,36 +11,9 @@ class Boost(BuiltinBoost):
 
     version("1.78.0.cms", commit="7f597ea02d8a714076157b4bf65fa8e5752b8468")
 
-    depends_on("zstd")
-    depends_on("xz")
-
     drop_patch("python_jam.patch")
 
     def determine_toolset(self, spec):
         if spec.satisfies("platform=darwin"):
             return "darwin"
         return super().determine_toolset(spec)
-
-    def determine_b2_options(self, spec, options):
-        threading_opts = super().determine_b2_options(spec, options)
-        if "+iostreams" in spec:
-            i = options.index("NO_LZMA=1")
-            options.pop(i + 1)
-            options.pop(i)
-            i = options.index("NO_ZSTD=1")
-            options.pop(i + 1)
-            options.pop(i)
-            options.extend(
-                [
-                    "-s",
-                    "ZSTD_INCLUDE=%s" % spec["zstd"].prefix.include,
-                    "-s",
-                    "ZSTD_LIBPATH=%s" % spec["zstd"].prefix.lib,
-                    "-s",
-                    "LZMA_INCLUDE=%s" % spec["xz"].prefix.include,
-                    "-s",
-                    "LZMA_LIBPATH=%s" % spec["xz"].prefix.lib,
-                ]
-            )
-
-        return threading_opts
