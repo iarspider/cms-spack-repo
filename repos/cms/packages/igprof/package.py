ddf53c6a4c830cbfc7820928f0c56b6023857125
Ivan Razumov <ivan.razumov@cern.ch>
2023-01-18 16:51:55 +0100

    Fix igprof and update env

--- repos/cms/packages/igprof/package.py
+++ repos/cms/packages/igprof/package.py
@@ -7,3 +7,12 @@ class Igprof(BuiltinIgprof):
     git = "https://github.com/cms-externals/igprof.git"
 
     version("5.9.16.cms", commit="2cd0b7d4fb21223b273c5a085cec2963c8206056")
+
+    @when("target=ppc64le:")
+    def build(self, spec, prefix):
+        return
+
+    @when("target=ppc64le:")
+    def install(self, spec, prefix):
+        with open(join_path(prefix, "dummy"), "w") as f:
+            f.write("## PLACEHOLDER ##")
